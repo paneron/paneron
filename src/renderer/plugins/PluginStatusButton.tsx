e036a177a0fa4333f0215f16760df04246311e1c
Anton Strogonoff <anton@strogonoff.name>
2020-10-16 15:25:34 +0900

    feat(plugins): better status button; allow upgrade

--- src/renderer/plugins/PluginStatusButton.tsx
+++ src/renderer/plugins/PluginStatusButton.tsx
@@ -5,21 +5,26 @@ import { jsx } from '@emotion/core';
 
 import React, { useState } from 'react';
 
-import { getPluginInfo, installPlugin, pluginsUpdated } from 'plugins';
-import { Button } from '@blueprintjs/core';
+import { getPluginInfo, installPlugin, pluginsUpdated, upgradePlugin } from 'plugins';
+import { Button, ButtonGroup } from '@blueprintjs/core';
 
 
 const PluginStatusButton: React.FC<{ id: string }> = function ({ id }) {
   const pluginInfo = getPluginInfo.renderer!.useValue({ id }, { id, title: id });
   const installedVersion = pluginInfo.value.installedVersion;
+  const latestVersion = pluginInfo.value.latestVersion;
   const [isBusy, setBusy] = useState(false);
 
   async function handleInstall() {
-    if (installedVersion) { return; }
+    if (installedVersion !== undefined && installedVersion === latestVersion) { return; }
 
     setBusy(true);
     try {
-      await installPlugin.renderer!.trigger({ id });
+      if (installedVersion === undefined) {
+        await installPlugin.renderer!.trigger({ id });
+      } else {
+        await upgradePlugin.renderer!.trigger({ id });
+      }
     } finally {
       setBusy(false);
     }
@@ -31,24 +36,32 @@ const PluginStatusButton: React.FC<{ id: string }> = function ({ id }) {
     }
   }, []);
 
-  if (pluginInfo.errors.length > 0) {
+  if (installedVersion === undefined && latestVersion === undefined) {
     const fetchError = pluginInfo.findError('FetchError');
     if (fetchError && fetchError.message.indexOf('registry.npmjs.org') >= 0) {
-      return <Button icon="offline" disabled>Cannot connect to plugin registry</Button>;
+      return <Button icon="offline" disabled>Cannot connect to package registry</Button>;
+    } else if (pluginInfo.isUpdating) {
+      return <Button loading />;
     } else {
-      return <Button icon="error" disabled>Cannot find plugin</Button>;
+      return <Button icon="error" disabled>Cannot find extension</Button>;
     }
   }
 
   return (
-    <Button
-        disabled={isBusy || installedVersion !== undefined}
-        loading={isBusy || pluginInfo.isUpdating}
-        intent="success"
-        onClick={handleInstall}
-        icon={installedVersion ? 'tick-circle' : 'download'}>
-      {installedVersion ? `Installed ${installedVersion}` : 'Install'}
-    </Button>
+    <ButtonGroup>
+      <Button
+          disabled={isBusy || pluginInfo.isUpdating || installedVersion === latestVersion || latestVersion === undefined}
+          loading={isBusy || pluginInfo.isUpdating}
+          intent="success"
+          onClick={handleInstall}
+          icon={installedVersion ? 'tick-circle' : 'download'}>
+        {installedVersion !== undefined
+          ? (installedVersion === latestVersion || latestVersion === undefined)
+            ? `Installed ${installedVersion}`
+            : `Upgrade from ${installedVersion} to ${latestVersion}`
+          : `Install ${latestVersion}`}
+      </Button>
+    </ButtonGroup>
   );
 };
 
