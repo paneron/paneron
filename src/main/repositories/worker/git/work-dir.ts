7c962319dba38a1e14cf5588b7dac9906e9cf835
Anton Strogonoff <anton@strogonoff.name>
2021-01-22 21:58:30 +0000

    refactor(repo worker): move discard uncommitted to git/work-dir

--- src/main/repositories/worker/git/work-dir.ts
+++ src/main/repositories/worker/git/work-dir.ts
@@ -44,8 +44,57 @@ const _delete: Git.WorkDir.Delete = async function ({ workDir }) {
 }
 
 
+const discardUncommitted: Git.WorkDir.DiscardUncommittedChanges = async function ({ workDir, pathSpec }) {
+  await git.checkout({
+    fs,
+    dir: workDir,
+    force: true,
+    filepaths: pathSpec ? [pathSpec] : undefined,
+  });
+  return { success: true };
+}
+
+
+// WARNING: Stages everything inside given working directory, then commits.
+// async function _commitAnyOutstandingChanges({ workDir, commitMessage, author }) {
+//   const repo = { fs, dir: workDir };
+// 
+//   // Add any modified or unstaged files (git add --no-all)
+//   const modifiedOrUntrackedPaths: string[] =
+//   await globby(['./**', './**/.*'], {
+//     gitignore: true,
+//     cwd: workDir,
+//   });
+//   for (const filepath of modifiedOrUntrackedPaths) {
+//     await git.add({ ...repo, filepath });
+//   }
+// 
+//   // Delete deleted files (git add -A)
+//   const removedPaths: string[] =
+//   await git.statusMatrix(repo).then((status) =>
+//     status.
+//       filter(([_1, _2, worktreeStatus]) => worktreeStatus < 1).
+//       map(([filepath, _1, _2]) => filepath)
+//   );
+//   for (const filepath of removedPaths) {
+//     await git.remove({ ...repo, filepath });
+//   }
+// 
+//   // Commit staged
+//   const newCommitHash = await git.commit({
+//     fs,
+//     dir: workDir,
+//     message: commitMessage,
+//     author,
+//   });
+// 
+//   return { newCommitHash };
+// },
+
+
 export default {
   validate,
   init,
   delete: _delete,
+  discardUncommitted,
 };
