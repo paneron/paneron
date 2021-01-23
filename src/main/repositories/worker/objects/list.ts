a4ae1f10d27de0ce39b9b954c84b7931efa6a713
Anton Strogonoff <anton@strogonoff.name>
2021-01-23 20:43:05 +0000

    refactor(repo worker): move object read function appropriately

--- src/main/repositories/worker/objects/list.ts
+++ src/main/repositories/worker/objects/list.ts
@@ -1,6 +1,3 @@
-import { readBuffers } from '../buffers/read';
-
-
 /* Given a generator of buffer paths, yields strings representing object paths.
    It may return the same object path more than once, since multiple buffers
    can represent a single object. */
@@ -13,18 +10,5 @@ export async function* listObjectPaths(
     if (objectPath !== null) {
       yield objectPath;
     }
-  }
-}
-
-
-/* Given a generator of object paths, yields objects.
-   Each object is created using the provided makeObject. */
-export async function* readObjects(
-  objectPaths: AsyncGenerator<string>,
-  makeObject: (fromBuffers: Record<string, Uint8Array>) => Record<string, any>,
-): AsyncGenerator<Record<string, any>> {
-  for await (const objectPath of objectPaths) {
-    const buffers = await readBuffers(objectPath);
-    yield makeObject(buffers);
   }
 }
--- src/main/repositories/worker/objects/read.ts
+++ src/main/repositories/worker/objects/read.ts
@@ -57,6 +57,19 @@ export async function readObject(
 }
 
 
+/* Given a generator of object paths, yields objects.
+   Each object is created using the provided makeObject. */
+export async function* readObjectsCold(
+  objectPaths: AsyncGenerator<string>,
+  makeObject: (fromBuffers: Record<string, Uint8Array>) => Record<string, any>,
+): AsyncGenerator<Record<string, any>> {
+  for await (const objectPath of objectPaths) {
+    const buffers = await readBuffers(objectPath);
+    yield makeObject(buffers);
+  }
+}
+
+
 export async function readObjectCold(
   rootPath: string,
   spec: SerializableObjectSpec,
