9e0803377355fb6134b13371e6bf703f60f1d742
Anton Strogonoff <anton@strogonoff.name>
2021-01-25 18:23:27 +0000

    refactor: refactor diffing functions

--- src/main/repositories/util.ts
+++ src/main/repositories/util.ts
@@ -17,30 +17,37 @@ export async function* diffDatasets<O extends Object | Buffer>(
   readItems: (path: string) => Promise<[ item1: O | null, item2: O | null ]>,
   compareItems: (item1: O, item2: O) => boolean,
 ): AsyncGenerator<[ path: string, changeStatus: DiffStatus ]> {
-
   for await (const p of paths) {
     const [item1, item2] = await readItems(p);
+    yield [p, diffItems(item1, item2, compareItems)];
+  }
+}
 
-    if (item1 === null || item2 === null) {
-      if (item1 !== item2) {
-        // Only one is null, so buffer either added or removed
-        if (item1 === null) {
-          yield [p, 'added'];
-        } else {
-          yield [p, 'removed'];
-        }
+export function diffItems<T>(
+  item1: T | null,
+  item2: T | null,
+  compareItems: (item1: T, item2: T) => boolean,
+): DiffStatus {
+
+  if (item1 === null || item2 === null) {
+    if (item1 !== item2) {
+      // Only one is null, so item either added or removed
+      if (item1 === null) {
+        return 'added';
       } else {
-        // Both are null
-        yield [p, 'unchanged'];
+        return 'removed';
       }
+    } else {
+      // Both are null, so item never existed (unexpected case)
+      return 'unchanged';
     }
+  }
 
-    else if (!compareItems(item1, item2)) {
-      yield [p, 'modified'];
-    }
+  else if (!compareItems(item1, item2)) {
+    return 'modified';
+  }
 
-    else {
-      yield [p, 'unchanged'];
-    }
+  else {
+    return 'unchanged';
   }
 }
