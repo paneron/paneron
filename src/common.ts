b19aacc220bb7c57d9234e1b7229ed265bcd916a
Anton Strogonoff <anton@strogonoff.name>
2021-03-13 07:58:56 +0000

    fix(persistent state reducer): add missing store/load state IPC hooks

--- src/common.ts
+++ src/common.ts
@@ -15,3 +15,19 @@ export const makeRandomID = makeEndpoint.main(
   <EmptyPayload>_,
   <{ id: string }>_,
 );
+
+
+export const storeState = makeEndpoint.main(
+  'storeState',
+  <{ key: string, newState: Record<string, any> }>_,
+  <{ success: true }>_,
+);
+
+
+export const loadState = makeEndpoint.main(
+  'loadState',
+  <{ key: string }>_,
+  <{ state: Record<string, any> }>_,
+);
+
+
