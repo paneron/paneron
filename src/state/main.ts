112cf446e57196bd8fb8ad7d71926ae9b6ceed63
Anton Strogonoff <anton@strogonoff.name>
2021-03-15 10:14:28 +0000

    chore(state): implement reset state IPC endpoints; export as functions for main

--- src/state/main.ts
+++ src/state/main.ts
@@ -3,7 +3,12 @@ import { app } from 'electron';
 import levelup from 'levelup';
 import leveldown from 'leveldown';
 import encode from 'encoding-down';
-import { loadState, storeState } from './common';
+import {
+  loadState as ipcLoadState,
+  storeState as ipcStoreState,
+  resetState as ipcResetState,
+  resetStateGlobal as ipcResetStateGlobal,
+} from './common';
 
 
 const STATE_STORAGE_PATH = path.join(app.getPath('userData'), 'state');
@@ -13,12 +18,54 @@ const stateStorage = levelup(encode(leveldown(STATE_STORAGE_PATH), {
 }));
 
 
-loadState.main!.handle(async ({ key }) => {
-  return await stateStorage.get(key);
+export async function loadState<S extends Record<string, any>>
+(key: string): Promise<S | undefined> {
+  try {
+    return await stateStorage.get(key);
+  } catch (e) {
+    if (e.type === 'NotFoundError') {
+      return undefined;
+    } else {
+      throw e;
+    }
+  }
+}
+
+
+export async function storeState<S extends Record<string, any>>
+(key: string, newState: S) {
+  await stateStorage.put(key, newState);
+}
+
+
+export async function resetState(key: string) {
+  await stateStorage.del(key);
+}
+
+
+export async function resetStateGlobal() {
+  await stateStorage.clear();
+}
+
+
+ipcLoadState.main!.handle(async ({ key }) => {
+  return { state: await loadState(key) };
 });
 
 
-storeState.main!.handle(async ({ key, newState }) => {
-  await stateStorage.put(key, newState);
+ipcStoreState.main!.handle(async ({ key, newState }) => {
+  await storeState(key, newState);
+  return { success: true };
+});
+
+
+ipcResetState.main!.handle(async ({ key }) => {
+  await resetState(key);
+  return { success: true };
+});
+
+
+ipcResetStateGlobal.main!.handle(async () => {
+  await resetStateGlobal();
   return { success: true };
 });
