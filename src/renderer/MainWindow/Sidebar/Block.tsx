69308e4e31dd855e449b535673a7dfc8973ba75b
Anton Strogonoff <anton@strogonoff.name>
2021-04-07 13:49:19 +0900

    chore(main window): implement sidebar components

---
+++ src/renderer/MainWindow/Sidebar/Block.tsx
@@ -0,0 +1,71 @@
+/** @jsx jsx */
+/** @jsxFrag React.Fragment */
+
+import { Button, ButtonGroup, Classes, Colors } from '@blueprintjs/core';
+import { jsx, css } from '@emotion/core';
+import React from 'react';
+
+
+export interface SidebarBlockConfig {
+  key: string
+  title: string | JSX.Element
+  content: JSX.Element
+}
+
+
+interface SidebarBlockProps {
+  block: SidebarBlockConfig
+  onExpand?: () => void
+  onCollapse?: () => void
+  expanded?: boolean
+}
+
+
+const SidebarBlock: React.FC<SidebarBlockProps> =
+function ({ expanded, onExpand, onCollapse, block }) {
+  return (
+    <div css={css`display: flex; flex-flow: column nowrap; background: ${Colors.LIGHT_GRAY2};`} className={Classes.ELEVATION_1}>
+      <div
+          css={css`
+            height: 24px; overflow: hidden; background: linear-gradient(to top, ${Colors.LIGHT_GRAY2}, ${Colors.LIGHT_GRAY3});
+            display: flex; flex-flow: row nowrap; align-items: center;
+            font-variation-settings: 'GRAD' 600, 'opsz' 20;
+            color: ${Colors.GRAY2};
+            text-shadow: 1px 1px 1px ${Colors.LIGHT_GRAY5};
+          `}>
+        <div css={css`flex: 1; font-size: 90%; padding: 5px 10px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;`}>
+          {block.title}
+        </div>
+        <ButtonGroup>
+          <Button minimal disabled={!onExpand} active={expanded} icon="expand-all" onClick={onExpand} />
+          <Button minimal disabled={!onExpand} active={!expanded} icon="collapse-all" onClick={onCollapse} />
+        </ButtonGroup>
+      </div>
+      {expanded
+        ? <div css={css`
+                overflow-x: hidden; overflow-y: auto;
+                padding: 5px;
+                box-shadow:
+                  inset 1px 1px 0 white,
+                  -1px -1px 0 ${Colors.GRAY4},
+                  -1px 0 0 ${Colors.GRAY4},
+                  0 -1px 0 ${Colors.GRAY4},
+                  inset -1px -1px 0 ${Colors.GRAY4},
+                  1px -1px 0 ${Colors.LIGHT_GRAY5},
+                  -1px 1px 0 ${Colors.LIGHT_GRAY5},
+                  1px 1px 0 ${Colors.LIGHT_GRAY5};
+                margin: 0 5px 5px 5px;
+                flex: 1;
+                line-height: 1.4;
+                font-size: 90%;
+                background: ${Colors.LIGHT_GRAY4};
+              `}>
+            {block.content}
+          </div>
+        : null}
+    </div>
+  );
+}
+
+
+export default SidebarBlock;
---
+++ src/renderer/MainWindow/Sidebar/index.tsx
@@ -0,0 +1,92 @@
+/** @jsx jsx */
+/** @jsxFrag React.Fragment */
+
+import { Button, ButtonGroup, Classes, Colors } from '@blueprintjs/core';
+import { jsx, css } from '@emotion/core';
+import React from 'react';
+import usePaneronPersistentStateReducer from 'state/usePaneronPersistentStateReducer';
+import SidebarBlock, { SidebarBlockConfig } from './Block';
+
+
+interface State {
+  blockState: { [blockTitle: string]: boolean } 
+}
+
+type Action =
+  | { type: 'expand-all' | 'collapse-all' }
+  | { type: 'collapse-one' | 'expand-one', payload: { blockKey: string } };
+
+interface SidebarProps {
+  stateKey: string
+  title: string | JSX.Element
+  blocks: SidebarBlockConfig[]
+
+  representsSelection?: boolean
+  /* Indicate via styling that sidebar is displaying details for a selected item. */
+
+  className?: string 
+}
+
+const Sidebar: React.FC<SidebarProps> =
+function ({ title, stateKey, blocks, representsSelection, className }) {
+  const [state, dispatch, stateLoaded] = usePaneronPersistentStateReducer<State, Action>(
+    (prevState, action) => {
+      switch (action.type) {
+        case 'expand-all':
+          return { blockState: blocks.map(b => ({ [b.key]: true })).reduce((prev, curr) => ({ ...prev, ...curr })) };
+        case 'collapse-all':
+          return { blockState: blocks.map(b => ({ [b.key]: false })).reduce((prev, curr) => ({ ...prev, ...curr })) };
+        case 'expand-one':
+          return { blockState: { ...prevState.blockState, [action.payload.blockKey]: true } };
+        case 'collapse-one':
+          return { blockState: { ...prevState.blockState, [action.payload.blockKey]: false } };
+        default:
+          throw new Error("Unexpected sidebar state");
+      }
+    },
+    { blockState: blocks.map(b => ({ [b.key]: true })).reduce((prev, curr) => ({ ...prev, ...curr })) },
+    null,
+    stateKey);
+
+  return (
+    <div css={css`display: flex; flex-flow: column nowrap;`} className={className}>
+      <div
+          css={css`
+            height: 24px; background: ${representsSelection ? Colors.BLUE2 : Colors.GRAY1};
+            color: white; display: flex; flex-flow: row nowrap; align-items: center;
+            overflow: hidden; z-index: 1;
+            font-variation-settings: 'GRAD' 500;
+          `}
+          className={Classes.ELEVATION_1}>
+
+        <div css={css`flex: 1; padding: 5px 10px; font-weight: bold; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;`}>
+          {title}
+        </div>
+
+        <ButtonGroup css={css`background: rgba(255, 255, 255, 0.6)`}>
+          <Button minimal title="Expand all" icon="expand-all" onClick={() => dispatch({ type: 'expand-all' }) } />
+          <Button minimal title="Collapse all" icon="collapse-all" onClick={() => dispatch({ type: 'collapse-all' })} />
+        </ButtonGroup>
+      </div>
+      <div css={css`flex: 1; overflow-x: hidden; overflow-y: auto; background: ${Colors.LIGHT_GRAY1};`}>
+      {stateLoaded
+        ? <>
+            {blocks.map((b, idx) =>
+              <SidebarBlock
+                key={idx}
+                expanded={state.blockState[b.key]}
+                block={b}
+                onCollapse={() => dispatch({ type: 'collapse-one', payload: { blockKey: b.key } })}
+                onExpand={() => dispatch({ type: 'expand-one', payload: { blockKey: b.key } })}
+              />
+            )}
+            <div css={css`font-size: 40px; text-align: center; color: ${Colors.LIGHT_GRAY4}`}>— ❧ —</div>
+          </>
+        : null}
+      </div>
+    </div>
+  );
+}
+
+
+export default Sidebar;
