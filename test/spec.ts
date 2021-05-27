c84df299d95a2c1850f86bbae2cb93813508d0ae
Anton Strogonoff <anton@strogonoff.name>
2021-05-27 12:13:40 +0000

    chore: test integration (WIP)

---
+++ test/spec.ts
@@ -0,0 +1,41 @@
+const Application = require('spectron').Application
+const assert = require('assert')
+const electronPath = require('electron') // Require Electron from the binaries included in node_modules.
+const path = require('path')
+
+
+describe('Application launch', function () {
+  this.timeout(10000)
+
+  beforeEach(function () {
+    this.app = new Application({
+      // Your electron path can be any binary
+      // i.e for OSX an example path could be '/Applications/MyApp.app/Contents/MacOS/MyApp'
+      // But for the sake of the example we fetch it from our node_modules.
+      path: electronPath,
+
+      // A workaround for “DevToolsActivePort file doesn't exist”,
+      // may or may not be necessary.
+      chromeDriverArgs: ['--remote-debugging-port=12209'],
+
+      // The following line tells spectron to look and use the main.js file
+      // and the package.json located 1 level above.
+      args: [path.join(__dirname, '..')]
+    })
+    return this.app.start()
+  })
+
+  afterEach(function () {
+    if (this.app && this.app.isRunning()) {
+      return this.app.stop()
+    }
+  })
+
+  it('shows an initial window', function () {
+    return this.app.client.getWindowCount().then(function (count: number) {
+      assert.equal(count, 1)
+      // Please note that getWindowCount() will return 2 if `dev tools` are opened.
+      // assert.equal(count, 2)
+    })
+  })
+})
