ca7c23147a84e34509a799181710c348bd3eeab5
Anton Strogonoff <anton@strogonoff.name>
2021-11-15 15:31:51 +0100

    feat: show installed ext. version (#28)

--- src/renderer/MainWindow/Nav/DatasetBreadcrumb.tsx
+++ src/renderer/MainWindow/Nav/DatasetBreadcrumb.tsx
@@ -4,6 +4,7 @@
 import { jsx } from '@emotion/react';
 import React, { useState } from 'react';
 import { IndexStatus } from '@riboseinc/paneron-extension-kit/types/indexes';
+import { getPluginInfo, pluginsUpdated } from 'plugins';
 import { describeIndex, indexStatusChanged } from 'datasets/ipc';
 import { DatasetInfo } from 'datasets/types';
 import { Breadcrumb } from './Breadcrumb';
@@ -22,6 +23,16 @@ export const DatasetBreadcrumb: React.FC<{
     datasetPath: datasetID,
   }, { status: initialDefaultDatasetIndexStatus });
 
+  const pluginInfoResp = getPluginInfo.renderer!.useValue(
+    { id: datasetInfo.type.id ?? 'N/A' },
+    { plugin: null });
+
+  const activeDatasetUIExtension = pluginInfoResp.value.plugin;
+
+  pluginsUpdated.renderer!.useEvent(async () => {
+    pluginInfoResp.refresh();
+  }, [datasetInfo.type.id]);
+
   indexStatusChanged.renderer!.useEvent(async ({ workingCopyPath, datasetPath, indexID, status }) => {
     if (workingCopyPath === workDir && datasetPath === datasetID && indexID === undefined) {
       setIndexStatus(status);
@@ -35,7 +46,18 @@ export const DatasetBreadcrumb: React.FC<{
       title={datasetInfo.title}
       icon={{ type: 'blueprint', iconName: "database" }}
       onClose={onClose}
-      status={<><code>{status.objectCount}</code> object(s) in dataset</>}
+      status={<>
+        <code>{status.objectCount}</code> object(s) in dataset
+        <br />
+        Dataset UI extension version:
+        {" "}
+        <code>
+          {activeDatasetUIExtension?.installedVersion ?? 'N/A'}
+        </code>
+        {activeDatasetUIExtension?.isLocal
+          ? <strong> (local)</strong>
+          : null}
+      </>}
       progress={status.progress}
     />
   );
