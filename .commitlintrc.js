f28db9e3714aa63df57441e8b07922f73c363de1
Jeffrey Lau <jeffrey.lau@ribose.com>
2023-01-20 19:39:56 +0800

    chore(husky): Not care about subject / type cases

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,5 +1,11 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: [
+    '@commitlint/config-conventional',
+    // '@commitlint/config-pnpm-scopes',
+  ],
   rules: {
+    // Do not care about cases!
+    'subject-case': [0, 'always', 'sentence-case'],
+    'type-case': [0, 'always', 'sentence-case'],
   },
 };
