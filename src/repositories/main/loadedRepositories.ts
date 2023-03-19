d9cd99feb62ad22291911fe5499259568b8feebf
Anton <anton@strogonoff.name>
2023-03-19 23:28:02 +0800

    docs(repos): document the `loadedRepositories` global

--- src/repositories/main/loadedRepositories.ts
+++ src/repositories/main/loadedRepositories.ts
@@ -14,6 +14,7 @@ import { getAuth } from './remoteAuth';
 import { makeSequential } from '../../utils';
 
 
+/** Holds currently loaded (synchronized) repositories. */
 const loadedRepositories: {
   [workingCopyPath: string]: {
     workers: RepoWorkers;
