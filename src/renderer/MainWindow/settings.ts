ced251c2bfd07a5dc4f8a7a85de4eaa2c2cd64cb
Anton <anton@strogonoff.name>
2023-04-29 20:54:06 +0800

    refactor(MainWindow): validate global settings
    on set/get

--- src/renderer/MainWindow/settings.ts
+++ src/renderer/MainWindow/settings.ts
@@ -1,5 +1,28 @@
-import type { Settings } from '@riboseinc/paneron-extension-kit/settings';
+import {
+  INITIAL_GLOBAL_SETTINGS,
+  isValidGlobalSettings,
+  type Settings,
+} from '@riboseinc/paneron-extension-kit/settings';
+
 import { loadState, storeState } from 'state/ipc';
+
+
+// TODO: Split functions for operating extension-specific settings and global settings
+// Reason: we should be strict about global settings type, but ideally avoid
+// a mess of generics.
+
+
+export const GLOBAL_SCOPE = 'global';
+
+
+export async function getSettings(scope: string): Promise<unknown> {
+  const { result}  = await loadState.renderer!.trigger({ key: `settings-${scope}` });
+  // Don’t validate non-global settings, we know nothing about them
+  return scope !== GLOBAL_SCOPE || isValidGlobalSettings(result.state)
+    ? result.state
+    : INITIAL_GLOBAL_SETTINGS;
+};
+
 
 export function useSettings<T extends Settings>(scope: string, initial: T) {
   const state = loadState.renderer!.useValue({
@@ -10,9 +33,14 @@ export function useSettings<T extends Settings>(scope: string, initial: T) {
     ?? initial;
   return {
     ...state,
-    value: { settings },
+    value: {
+      settings: scope !== GLOBAL_SCOPE || isValidGlobalSettings(settings)
+        ? settings
+        : INITIAL_GLOBAL_SETTINGS,
+    },
   };
 };
+
 
 export async function updateSetting(scope: string, { key, value }: { key: string, value: any }) {
   // Fetch all settings firtst
@@ -20,10 +48,23 @@ export async function updateSetting(scope: string, { key, value }: { key: string
     key: `settings-${scope}`,
   })).result?.state as (Settings | undefined) ?? {};
 
+  const newSettings = { ...settings, [key]: value };
+
+  if (scope === GLOBAL_SCOPE && !isValidGlobalSettings(newSettings)) {
+    try {
+      await storeState.renderer!.trigger({
+        key: `settings-${scope}`,
+        newState: INITIAL_GLOBAL_SETTINGS,
+      });
+    } finally {
+      throw new Error("Unexpected global settings structure obtained. Reset all settings to defaults just in case.");
+    }
+  }
+
   // Refresh given setting
   await storeState.renderer!.trigger({
     key: `settings-${scope}`,
-    newState: { ...settings, [key]: value },
+    newState: newSettings,
   });
 
   return { success: true as const };
