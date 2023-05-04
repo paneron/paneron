a48debbba8de4b97a46f0bb1618bc347d28a75f1
Anton <anton@strogonoff.name>
2023-05-04 18:36:18 +0800

    refactor(gui): support className prop on RecentDatasets

--- src/renderer/MainWindow/WelcomeScreen/RecentDatasets.tsx
+++ src/renderer/MainWindow/WelcomeScreen/RecentDatasets.tsx
@@ -7,11 +7,14 @@ import { Menu } from '@blueprintjs/core';
 import { listRecentlyOpenedDatasets } from 'datasets/ipc';
 import DatasetMenuItem from './DatasetMenuItem';
 
-const RecentDatasets: React.FC<{ onOpenDataset?: (workDir: string, dsID: string) => void; }> = function ({ onOpenDataset }) {
+const RecentDatasets: React.FC<{
+  onOpenDataset?: (workDir: string, dsID: string) => void;
+  className?: string;
+}> = function ({ onOpenDataset, className }) {
   const recentDatasetsResp = listRecentlyOpenedDatasets.renderer!.useValue({}, { datasets: [] });
   const datasets = recentDatasetsResp.value.datasets;
   return (
-    <Menu>
+    <Menu className={className}>
       {datasets.map(({ workDir, datasetID }) => <DatasetMenuItem
         key={`${workDir}-${datasetID}`}
         workDir={workDir}
