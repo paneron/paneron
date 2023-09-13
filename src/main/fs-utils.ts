2d18cf6250cb8680543057aeab2cf2c18c994d5d
Anton <anton@strogonoff.name>
2023-09-13 19:09:14 +0800

    refactor: POSIX/Win32 path conversion utils
    Working on #110

--- src/main/fs-utils.ts
+++ src/main/fs-utils.ts
@@ -1,4 +1,43 @@
+/** Platform-specific FS-related utils. */
+
 import { statSync, readFileSync } from 'fs';
+
+import { stripLeadingSlash as posixStripLeadingSlash } from '../utils';
+
+import nodePath from 'path';
+
+
+/**
+ * Convert platform-specific path returned by Node to POSIX format used internally.
+ * No-op on POSIX systems.
+ *
+ * NOTE: For performance, doesn’t check that given path is POSIX or not.
+ * If you have obtained a path using Node’s fs/path layers,
+ * wrap it in this function if you need to pass that path anywhere else.
+ */
+export const posixifyPath: (platformSpecificPath: string) => string =
+  process.platform === 'win32'
+    ? function posixifyPath(aPath) { return aPath.split(nodePath.sep).join(nodePath.posix.sep); }
+    : function posixifyPathNoOp(aPath) { return aPath; }
+
+/**
+ * Convert POSIX format path to platform-specific format for filesystem access.
+ * No-op on POSIX systems.
+ *
+ * NOTE: For performance, doesn’t check that given path is POSIX or not.
+ * Wrap paths in this function when reading data from filesystem,
+ * unless you have obtained platform-specific path within the same function.
+ */
+export const deposixifyPath: (posixPath: string) => string =
+  process.platform === 'win32'
+    ? function deposixifyPath(aPath) { return aPath.split(nodePath.posix.sep).join(nodePath.sep); }
+    : function deposixifyPathNoOp(aPath) { return aPath; };
+
+
+export const stripLeadingSlashPlatformSpecific: (maybeSlashPrependedPath: string) => string =
+  process.platform === 'win32'
+    ? function stripLeadingSlashWin32(aPath) { return aPath.replace(/^\\/, ''); }
+    : posixStripLeadingSlash;
 
 
 export function checkPathIsOccupied(absolutePath: string): boolean {
