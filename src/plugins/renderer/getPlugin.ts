10a7c1cd22ed7a57d8fade214bdcc3db2d5f460c
Anton <anton@strogonoff.name>
2023-09-26 20:38:08 +0800

    refactor: pass `console` to extensions differently

--- src/plugins/renderer/getPlugin.ts
+++ src/plugins/renderer/getPlugin.ts
@@ -96,7 +96,12 @@ export default async function getPlugin(id: string, version: string | undefined)
 
 
 const global = {
-  console,
+  console: {
+    debug: (...args: any[]) => console.debug(...args),
+    log: (...args: any[]) => console.log(...args),
+    warn: (...args: any[]) => console.warn(...args),
+    error: (...args: any[]) => console.error(...args),
+  },
   Object,
   Array,
   ArrayBuffer,
